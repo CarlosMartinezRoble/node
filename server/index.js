a02aa34aeb9f7b4d79d0e085d5051f15f73634bd
CarlosMartinezRobles <90333573+CarlosMartinezRobles@users.noreply.github.com>
2022-01-24 12:49:08 +0100

    estructura de proyecto: express, nodemon, cors, morgan, mongoose(fall MongoAtlas)

--- server/index.js
+++ server/index.js
@@ -1,12 +1,33 @@
- const express = require("express");
-
+ const { use } = require("bcrypt/promises");
+const express = require("express");
+const res = require("express/lib/response");
+const cors=require("cors");
+const logger=require("morgan")
 // const{connect}= require("./app/config/database");
 
 // connect();
-
-const app= express();
 const PORT=3000;
+const app= express();
+
+app.use((req,res,next)=>{
+    res.header('Access-Control-Alow-Methods','GET,PUT,POST,DELETE');
+    res.header('Access-Control-Alow-Credentials',true);
+    res.header('Access-Control-Alow-Header',"Content-Type");
+    next();
+})
+ 
+app.use(cors({
+    origin:[`http://localhost:3000`,`http://localhost:4000`],
+    credentials:true, 
+    })
+);
+app.use(express.json());  
+app.use(express.urlencoded({extended: true}));
+
+app.use(logger("dev"));
+
 
 app.listen(PORT, () => {
     console.log(`Server running in http://localhost:${PORT}`);
-  });
+});
+
