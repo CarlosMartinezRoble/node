fd582680c274d836ac9e91d3032266d3494d38ac
CarlosMartinezRobles <90333573+CarlosMartinezRobles@users.noreply.github.com>
2022-01-25 12:15:45 +0100

    routs

--- server/index.js
+++ server/index.js
@@ -1,14 +1,20 @@
- const { use } = require("bcrypt/promises");
+const { use } = require("bcrypt/promises");
 const express = require("express");
 const res = require("express/lib/response");
 const cors=require("cors");
 const logger=require("morgan")
 // const{connect}= require("./app/config/database");
-
 // connect();
 const PORT=3000;
 const app= express();
 
+//routes .
+const user=require("./app/api/routes/user.routes");
+const character=require("./app/api/routes/character.routes");
+const location=require("./app/api/routes/location.routes");
+const home=require("./app/api/routes/home.routes");
+
+//headers.e
 app.use((req,res,next)=>{
     res.header('Access-Control-Alow-Methods','GET,PUT,POST,DELETE');
     res.header('Access-Control-Alow-Credentials',true);
@@ -27,10 +33,15 @@ app.use(express.urlencoded({extended: true}));
 app.use(logger("dev"));
 
 
+app.use("/", home);
+app.use("/user", user);
+app.use("/character", character);
+app.use("/location", location);
+
 app.listen(PORT, () => {
     console.log(`Server running in http://localhost:${PORT}`);
 });
 
 
 
-//video 21:11 del segundo video de antonio
+//video 28:07 del segundo video de antonio
