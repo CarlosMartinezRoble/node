17b0adbc7a070c92b247d3736558472af84c342c
CarlosMartinezRobles <90333573+CarlosMartinezRobles@users.noreply.github.com>
2022-01-25 20:52:59 +0100

    control de errores

--- server/index.js
+++ server/index.js
@@ -7,14 +7,16 @@ const logger=require("morgan")
 // connect();
 const PORT=3000;
 const app= express();
+const HTTPSTATUSCODE= require ("./app/utils/httpStatusCode");   
 
 //routes .
 const user=require("./app/api/routes/user.routes");
 const character=require("./app/api/routes/character.routes");
 const location=require("./app/api/routes/location.routes");
 const home=require("./app/api/routes/home.routes");
+const { json } = require("express/lib/response");
 
-//headers.e
+//headers
 app.use((req,res,next)=>{
     res.header('Access-Control-Alow-Methods','GET,PUT,POST,DELETE');
     res.header('Access-Control-Alow-Credentials',true);
@@ -37,6 +39,20 @@ app.use("/", home);
 app.use("/user", user);
 app.use("/character", character);
 app.use("/location", location);
+
+//control de errores
+app.use((req,res,next)=>{
+    let err= new Error;
+    err.status = 404;
+    err.message= HTTPSTATUSCODE[404];
+    next(err);
+});
+app.use((err, req,res,next)=>{
+    return res.status(err.status||500),json(err.message||"unexpected error");
+});
+
+// ocultar node.js
+app.disable("x-powered-by")
 
 app.listen(PORT, () => {
     console.log(`Server running in http://localhost:${PORT}`);
