0d10848cf15100a5b757ccd7a7ef0c97549870d7
Carlos Martinez <carlos.martinez.robles@the-cocktail.com>
2022-09-26 12:33:26 +0200

    booksroute

--- ApiReSTCurso/app.js
+++ ApiReSTCurso/app.js
@@ -2,13 +2,21 @@
  const express = require ('express');
 
  const app = express(); 
-
+ 
+ const bookRouter = express.Router();
  const port = process.env.PORT || 3000; 
 
  app.get ('/', ( req, res ) => {
-   res.send ( ' Welcome to my API!' )
+   res.send ( ' Welcome to my Nodemon API!' )
+ });
+bookRouter.route('/books')
+ .get (( req, res)=> {
+    const response = { hello :'its my API' };
+    res.jason(response);
  });
 
+ app.use('/api', bookRouter);
+
 app.listen(port, ()=>{
-  console.log('Runing on port ' + port);
+  console.log(    `Runing on port ${port}`);
 });
